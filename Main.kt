a93f9bb3031437892372717b8277ba628822aa5f
Chris Van Ry <christophervanry@gmail.com>
2022-07-28 22:55:14 -0600

    Chapter 3 complete

--- Main.kt
+++ Main.kt
@@ -7,7 +7,32 @@ fun main() {
     var playerLevel = 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            // Check whether diplomacy is an option
+            val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+            6 -> "Locate the enchanted sword."
+            7 -> "Recover the long-lost artifact of creation."
+            8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+            else -> "There are no quests right now."
+        }
+
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel += 1
     println(playerLevel)
 }
