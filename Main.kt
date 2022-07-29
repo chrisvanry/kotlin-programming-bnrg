98564ac3ca18ef626b2bcb2ab157f88e84a2c7ab
Chris Van Ry <christophervanry@gmail.com>
2022-07-29 02:18:17 -0600

    Working on chapter 4

--- Main.kt
+++ Main.kt
@@ -1,15 +1,34 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 5
 
 fun main() {
     println("The hero announces her presence to the world.")
 
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
 
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
+    readBountyBoard()
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+
+    readBountyBoard()
+}
+
+private fun readBountyBoard() {
+    println("The hero approaches the bounty board. It reads:")
+    println(obtainQuest(playerLevel, "paladin", true, false))
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String,
+    hasBefriendedBarbarians: Boolean,
+    hasAngeredBarbarians: Boolean
+): String {
     val quest: String = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
@@ -21,18 +40,11 @@ fun main() {
                 "Save the town from the barbarian invasions."
             }
         }
-            6 -> "Locate the enchanted sword."
-            7 -> "Recover the long-lost artifact of creation."
-            8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-            else -> "There are no quests right now."
-        }
 
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
-
-    println("Time passes...")
-    println("The hero returns from her quest.")
-
-    playerLevel += 1
-    println(playerLevel)
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> "There are no quests right now."
+    }
+    return quest
 }
